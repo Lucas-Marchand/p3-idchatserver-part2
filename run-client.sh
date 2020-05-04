203fb5f909776d1213795acd9fdad32641ff9e6b
Lucas Marchand <Lucas_Marchand@Hotmail.com>
2020-05-03 22:39:32 -0400

    trash

--- run-client.sh
+++ run-client.sh
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 #!/bin/bash
 java -cp ./src/:./inc/commons-cli-1.4/commons-cli-1.4.jar IdClient -s 172.17.0.1 172.17.0.2 172.17.0.3 -c lucas -p marchand -n 5654
+=======
+java -cp ./src/:./inc/commons-cli-1.4/commons-cli-1.4.jar IdClient -s 172.17.0.2 172.17.0.3 -c lucas -p marchand -n 5654
+>>>>>>> parent of 5a9df22... Added #!/bin/bash to scripts, .gitignore
--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 #!/bin/bash
 java -cp ./src/:./inc/commons-cli-1.4/commons-cli-1.4.jar -Djava.security.policy=mysecurity.policy IdServer -i 172.17.0.1 172.17.0.2 172.17.0.3 -n 5654 --verbose
+=======
+java -cp ./src/:./inc/commons-cli-1.4/commons-cli-1.4.jar -Djava.security.policy=mysecurity.policy IdServer -i 172.17.0.1 172.17.0.2 172.17.0.2 -n 5654 --verbose
+>>>>>>> parent of 5a9df22... Added #!/bin/bash to scripts, .gitignore
